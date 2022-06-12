e7b50a6b0587f01febe8e9fee4a9794ca8625bee
beeerlian <naufal.berlian.99@gmail.com>
2022-06-12 21:17:36 +0700

    fix upload to cloud bucket bug

--- controllers/ai.controller.js
+++ controllers/ai.controller.js
@@ -64,7 +64,7 @@ exports.predictImage = async (req, res) => {
 
               }
               resHelper.successResponse(res, "predict success", jsonRest['data'])
-              sendNotification(req, `Object detected`, jsonRest['data'], image)
+              sendNotification(req, `Object detected`, jsonRest['data'], data)
               // await db.updateNotification(notif.toObj());
 
        } catch (error) {
