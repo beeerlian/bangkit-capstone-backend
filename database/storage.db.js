986375a81bcf18aa4b2d4aeba9a2fc7bf41aded4
beeerlian <naufal.berlian.99@gmail.com>
2022-06-12 21:52:08 +0700

    update predict flow

--- database/storage.db.js
+++ database/storage.db.js
@@ -2,19 +2,23 @@ const gcsBucket = require("./cloudStorage")
 
 exports.saveImage = (image) => new Promise((resolve, reject) => {
        console.log("uploading " + image.originalname + " to " + gcsBucket.name)
-       const date = new Date().getTime();
-       const file = gcsBucket.file(date + '_' + image.originalname.replace(/ /g, "_"));
-       const fileStream = file.createWriteStream({
-              resumable: false
-       });
-       let url
-       fileStream.on('finish', () => {
-              url = `https://storage.googleapis.com/${gcsBucket.name}/${file.name}`;
-              console.log('file stream finish ' + url);
-              resolve(url);
-       })
-              .on('error', () => {
-                     reject(`upload image failed, problem occur`);
+       try {
+              const date = new Date().getTime();
+              const file = gcsBucket.file(date + '_' + image.originalname.replace(/ /g, "_"));
+              const fileStream = file.createWriteStream({
+                     resumable: false
+              });
+              let url
+              fileStream.on('finish', () => {
+                     url = `https://storage.googleapis.com/${gcsBucket.name}/${file.name}`;
+                     console.log('file stream finish ' + url);
+                     resolve(url);
               })
-              .end(image.buffer);
+                     .on('error', () => {
+                            reject(`upload image failed, problem occur`);
+                     })
+                     .end(image.buffer);
+       } catch (error) {
+              console.log("[error gcs upload] : " + error.message)
+       }
 });
